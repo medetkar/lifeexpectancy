aec8254d35675c2ee83ce7a89bb692adfc7f9fbe
medetkar <medetkar@gmail.com>
2022-04-16 23:59:37 +0300

    Gesture detector eklendi

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -4,13 +4,14 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'myWidgets/genderColumn.dart';
 import 'myWidgets/myColumn.dart';
 
-
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
 class _InputPageState extends State<InputPage> {
+  String seciliCinsiyet;
+  //Color butonRenk;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,17 +60,37 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  flex: 1,
                   child: MyContainer(
-                    child: GenderColumn(cinsiyet: 'Kadın',icon: FontAwesomeIcons.venus,),
-                    renk: Colors.purple,
+                    onTap: () {
+                      setState(
+                        () {
+                          seciliCinsiyet = 'KADIN';
+                        },
+                      );
+                    },
+                    child: GenderColumn(
+                      cinsiyet: 'KADIN',
+                      icon: FontAwesomeIcons.venus,
+                    ),
+                    renk:
+                        seciliCinsiyet == 'KADIN' ? Colors.green : Colors.white,
                   ),
                 ),
                 Expanded(
-                  flex: 1,
                   child: MyContainer(
-                    child: GenderColumn(cinsiyet: 'Erkek',icon: FontAwesomeIcons.mars,),
-                    renk: Colors.blue[900],
+                    onTap: () {
+                      setState(
+                        () {
+                          seciliCinsiyet = 'ERKEK';
+                        },
+                      );
+                    },
+                    child: GenderColumn(
+                      cinsiyet: 'ERKEK',
+                      icon: FontAwesomeIcons.mars,
+                    ),
+                    renk:
+                        seciliCinsiyet == 'ERKEK' ? Colors.green : Colors.white,
                   ),
                 ),
               ],
--- lib/myWidgets/myColumn.dart
+++ lib/myWidgets/myColumn.dart
@@ -3,16 +3,20 @@ import 'package:flutter/material.dart';
 class MyContainer extends StatelessWidget {
   final Color renk;
   final Widget child;
-  MyContainer({this.renk = Colors.white, this.child});
+  final Function onTap;
+  MyContainer({this.renk = Colors.white, this.child, this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: child,
-      margin: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: renk,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child: child,
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: renk,
+        ),
       ),
     );
   }
